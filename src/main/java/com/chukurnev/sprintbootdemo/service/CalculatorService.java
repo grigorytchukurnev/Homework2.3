25f6859ffc8f8fda63e78eefffe763f339faf068
Acer <chukurnev@mail.ru>
2023-03-31 02:23:25 +0400

    Cделано ДЗ

---
+++ src/main/java/com/chukurnev/sprintbootdemo/service/CalculatorService.java
@@ -0,0 +1,9 @@
+package com.chukurnev.sprintbootdemo.service;
+
+public interface CalculatorService {
+    Integer plus(Integer num1,Integer num2);
+    Integer minus(Integer num1,Integer num2);
+    Integer multiply(Integer num1,Integer num2);
+    Integer divide(Integer num1,Integer num2);
+
+}
